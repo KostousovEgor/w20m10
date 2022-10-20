b5c0627c0ed6cfb0f0abf72555f59e5bc2866d98
KostousovEgor <106836404+KostousovEgor@users.noreply.github.com>
2022-10-21 00:07:15 +0500

    Add files via upload

---
+++ postemail.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="sandmail.php" method="POST">
+        <select name="subject">
+            <option disabled selected>Тема письма</option>
+            <option value="1">Вопрос по уроку</option>
+            <option value="2">Личный вопрос</option>
+            <option value="3">Благодарность</option>
+        </select>
+        <input type="email" name="to" placeholder="Введите email получателя" maxlength="50" required>
+        <input type="email" name="email" placeholder="Введите ваш email" maxlength="50" required>
+        <textarea name="message" placeholder="Введите сообщение" maxlength="150" required></textarea>
+        <img src="capcha1.jpg">
+        <input type="number" name="capcha" placeholder="Введите ответ" maxlength="3" required>
+        <input type="submit" value="Отправить письмо">
+    </form>
+</body>
+
+</html>
